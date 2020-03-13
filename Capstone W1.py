35bbfec6bed7b3004388e69e04d6b8460951525e
Ansh2523 <62149206+Ansh2523@users.noreply.github.com>
2020-03-13 22:13:29 +0530

    Add files via upload

---
+++ Capstone W1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook is being prepared in accordance to the final capstone assignemt of IBM Data Science Course
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello capstone project course.")
+
